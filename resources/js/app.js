4d287eeb030d14f609db7bd348fd378300b2f119
Gleison Rocha <glucasmr@gmail.com>
2022-06-18 02:47:13 -0300

    vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,22 @@
 import './bootstrap';
 
-import Vue from 'vue'
+import { createApp } from 'vue'
 
 import App from './vue/app'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add( faPlusSquare, faTrash )
+
+
+export const eventBus = createApp(App)
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
 const app = new Vue({
     el: '#app',
-    components: { App }
-
-});
+    components: { App } 
+}
+);
